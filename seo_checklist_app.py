6ba509592eaeeefeb48e488c6eda034ab9b5ae6d
Aitoral92 <103137933+Aitoral92@users.noreply.github.com>
2024-04-10 11:30:40 +0200

    Update seo_checklist_app.py

--- seo_checklist_app.py
+++ seo_checklist_app.py
@@ -158,7 +158,7 @@ def main():
                        'Cache-Control':'no-cache',
                        'Pragma':'no-cache',
                        'x-seo-crawler':'bebb296ec7bbf3a1cd81f4863e069de6'}
-            get_url = requests.get(url)
+            get_url = requests.get(url, headers)
             st.info(f"prueba{get_url}")
             soup = BeautifulSoup(get_url.content, "html.parser")
             st.info(f"prueba{soup}")
