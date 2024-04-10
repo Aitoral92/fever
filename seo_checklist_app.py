bb2d617432fb52905ce32925acb3774527326e36
Aitoral92 <103137933+Aitoral92@users.noreply.github.com>
2024-04-10 16:59:00 +0200

    Update seo_checklist_app.py
    user agent

--- seo_checklist_app.py
+++ seo_checklist_app.py
@@ -154,7 +154,7 @@ def main():
     if st.button("Analyze"):
         if url:
             headers = {
-                # 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
                 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Encoding':'gzip',
                 'Cache-Control':'no-cache',
