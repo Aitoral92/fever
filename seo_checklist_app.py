264ea36b5fde9c0827efa48877948e63e4c0031f
Aitoral92 <103137933+Aitoral92@users.noreply.github.com>
2024-04-10 11:28:40 +0200

    Update seo_checklist_app.py
    custom header

--- seo_checklist_app.py
+++ seo_checklist_app.py
@@ -7,7 +7,6 @@ from urllib.parse import urlparse
 from urllib.request import urlopen as uReq
 import streamlit as st
 import json
-import time
 
 # def count_hyphens_and_digits_in_url(url):
 #     hyphen_count = url.count("-")
@@ -154,6 +153,11 @@ def main():
 
     if st.button("Analyze"):
         if url:
+            headers = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                       'Accept-Encoding':'gzip',
+                       'Cache-Control':'no-cache',
+                       'Pragma':'no-cache',
+                       'x-seo-crawler':'bebb296ec7bbf3a1cd81f4863e069de6'}
             get_url = requests.get(url)
             st.info(f"prueba{get_url}")
             soup = BeautifulSoup(get_url.content, "html.parser")
