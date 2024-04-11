4c65fc969511c62afd543ec63fa9d318647436df
Aitoral92 <103137933+Aitoral92@users.noreply.github.com>
2024-04-11 16:27:11 +0200

    Update seo_checklist_app.py

--- seo_checklist_app.py
+++ seo_checklist_app.py
@@ -169,7 +169,7 @@ def main():
             # soup = BeautifulSoup(get_url.content, "html.parser")
             # st.info(f"prueba{soup}")
             
-            x_seo_crawler="custom_header"
+            x_seo_crawler=st.write("x-seo-crawler:", st.secrets["custom_header"])
             ses = requests_html.HTMLSession()
             headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
                        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
